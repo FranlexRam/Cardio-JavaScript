63f72c196253028d9b1723d39ea02f8f0ced43a3
Franlex Ramirez <franlex.ramirez@gmail.com>
2022-11-18 05:43:39 -0400

    Cardio JS ejercicio 4 y 5

--- cardio4.js
+++ cardio4.js
@@ -1,5 +1,14 @@
 /*
-4) Escribir un programa que pida un numero entero mayor que 1 y que escriba si el numero es primo o no.
-
-Numero primo: solo es divisible por si mismo y por la unidad.
+4) Escribir un programa que permita ir introduciendo una serie indeterminada de numeros mientras su suma no supere 50.
+Cuando esto suceda, se debe mostrar el total acumulado y el contador de cuantos numeros se han introducido.
 */
+
+let sum=0;
+let counter=0;
+
+while (sum <= 50) {
+    sum = sum + parseInt(prompt('Introduce un numero:'));
+    counter++;    
+}
+
+alert(`La suma total es de ${sum}.\nTotal de numeros introducidos: ${counter}`);
